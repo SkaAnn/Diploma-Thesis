0d2a002c491d0a6f56dfa70c4b57d86a4462f80f
SkaAnn <xskachova@stuba.sk>
2021-02-22 16:46:55 +0100

    Header and Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer style={{ background: '#593196', color: 'white', minHeight: '7rem' }}>
+            <Container >
+                <Row style={{ padding: '1rem' }}>
+                    <Col className='text-center py-3' style={{ lineHeight: '2rem' }}>
+                        Diplomová práca <br />
+                        Copyright &copy; Bc. Anna Skachová, 2021 <br />
+                        <a href='https://github.com/SkaAnn/'>
+                            <i className="fas fa-envelope fa-lg" style={{ color: 'white' }} ></i>
+                        </a>&nbsp; &nbsp;
+                        <a href='https://github.com/SkaAnn/' style={{ color: 'white' }}>
+                            <i className="fab fa-github fa-lg"></i>
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
+// #3f51b5
+// https://bootsnipp.com/snippets/mMynR
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <Navbar className='navbar-dark indigo' expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">DP-2021</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/login"><i className='fas fa-user' /> &nbsp; Prihlásiť sa</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 
