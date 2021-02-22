fc7c153a2444f7e91f653dcd89b1221ba828c8d3
SkaAnn <xskachova@stuba.sk>
2021-02-22 11:42:14 +0100

    Setup connection MobgoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 
+import connectDB from './config/db.js'
+
+// Connect to database
+connectDB() 
+
 const app = express()
 const apiPort = 5000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
