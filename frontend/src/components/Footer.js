847799d78b38ed5bedc9d5bac3217dfad136b3c2
SkaAnn <xskachova@stuba.sk>
2021-03-17 12:08:30 +0100

    Footer styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,21 +3,25 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer style={{ background: '#593196', color: 'white', minHeight: '7rem' }}>
+        <footer style={{ background: '#78f5d6', minHeight: '7rem' }}>
             <Container >
-                <Row style={{ padding: '1rem' }}>
-                    <Col className='text-center py-3' style={{ lineHeight: '2rem' }}>
-                        Diplomová práca <br />
-                        Copyright &copy; Bc. Anna Skachová, 2021 <br />
-                        <a href='https://github.com/SkaAnn/'>
-                            <i className="fas fa-envelope fa-lg" style={{ color: 'white' }} ></i>
-                        </a>&nbsp; &nbsp;
-                        <a href='https://github.com/SkaAnn/' style={{ color: 'white' }}>
-                            <i className="fab fa-github fa-lg"></i>
+                <Row className='fs-14px py-4 px-3'>
+                    <Col className='col-md-4' style={{ lineHeight: '2rem' }}>
+                        <span className='text-uppercase'> Diplomová práca </span> <br/>
+                        <span className='fw-600'>Bc. Anna Skachová </span> <br/>
+
+                        <a href='https://github.com/SkaAnn/' style={{color: 'black'}}> 
+                            <i className="fas fa-at fa-lg mr-1"></i> skachova@stuba.sk
+                        </a> <br/>
+                        <a href='https://github.com/SkaAnn/' style={{color: 'black'}}>
+                            <i className="fab fa-github fa-lg  mr-1"></i> SkaAnn
                         </a>
                     </Col>
                 </Row>
             </Container>
+            <div className='text-center py-2' style={{ background: 'rgb(112 227 199)'}}>
+                &copy; {new Date().getFullYear()}
+            </div>
         </footer>
     )
 }
