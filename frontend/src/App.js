28e1367074c32986ce7de565f012ab9038d78c50
SkaAnn <xskachova@stuba.sk>
2021-03-25 11:46:29 +0100

    404 Screen frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -17,6 +17,7 @@ import FavoriteProductsScreen from './screens/FavoriteProductsScreen'
 import RegisterScreenNew from './screens/RegisterScreenNew'
 import ProfileUpdateScreenNew from './screens/ProfileUpdateScreenNew'
 import ProductCreateScreenNew from './screens/ProductCreateScreenNew'
+import My404Screen from './screens/My404Screen'
 
 const App = () => {
   return (
@@ -24,25 +25,28 @@ const App = () => {
       <Header />
       <main className='py-3'>
         {/* <Container> */}
-        <Route path='/login' component={LoginScreen} />
-        <Route path='/register' component={RegisterScreenNew} />
-        <Route path='/products/user/:id' component={UserProductsScreen} exact />
-        <Route path='/products/user/:id/:pageNumber' component={UserProductsScreen} exact />
-        <Route path='/product/:id' component={ProductScreen} />
-        <Route path='/user/my/favorites' component={FavoriteProductsScreen} exact />
-        <Route path='/user/my/favorites/:pageNumber' component={FavoriteProductsScreen} exact />
-        <Route path='/user/my/profile' component={ProfileScreen} exact />
-        <Route path='/user/my/profile/update' component={ProfileUpdateScreenNew} exact />
-        <Route path='/user/my/profile/page/:pageNumber' component={ProfileScreen} exact />
-        <Route path='/user/my/product/create' component={ProductCreateScreenNew} />
-        <Route path='/user/product/:id/edit' component={ProductEditScreen} />
-        <Route path='/sort/:sortKey' component={HomeScreen} exact />
-        <Route path='/search/:keyword' component={HomeScreen} exact />
-        <Route path='/page/:pageNumber' component={HomeScreen} exact />
-        <Route path='/sort/:sortKey/page/:pageNumber' component={HomeScreen} exact />
-        <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
-        <Route path='/sort/:sortKey/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
-        <Route path='/' component={HomeScreen} exact />
+        <Switch>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreenNew} />
+          <Route path='/products/user/:id' component={UserProductsScreen} exact />
+          <Route path='/products/user/:id/:pageNumber' component={UserProductsScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/user/my/favorites' component={FavoriteProductsScreen} exact />
+          <Route path='/user/my/favorites/:pageNumber' component={FavoriteProductsScreen} exact />
+          <Route path='/user/my/profile' component={ProfileScreen} exact />
+          <Route path='/user/my/profile/update' component={ProfileUpdateScreenNew} exact />
+          <Route path='/user/my/profile/page/:pageNumber' component={ProfileScreen} exact />
+          <Route path='/user/my/product/create' component={ProductCreateScreenNew} />
+          <Route path='/user/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/sort/:sortKey' component={HomeScreen} exact />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/sort/:sortKey/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/sort/:sortKey/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/' component={HomeScreen} exact />
+          <Route component={My404Screen} />
+        </Switch>
         {/* </Container> */}
       </main>
       <Footer />
---
+++ frontend/src/screens/My404Screen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const My404Screen = () => {
+    return (
+        <Container>
+
+            <h1 className='fw-500'
+                style={{
+                    margin: '0',
+                    position: 'absolute',
+                    top: '50%',
+                    // -ms-transform: translateY(-50%);
+                    transform: 'translateY(-120%)'
+                }}> 404 Táto stránka neexistuje </h1>
+        </Container>
+    )
+}
+
+export default My404Screen
