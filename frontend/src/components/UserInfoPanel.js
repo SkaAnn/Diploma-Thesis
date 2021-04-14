109dbe300f75e21657a49fce63885bb40da2a160
SkaAnn <xskachova@stuba.sk>
2021-04-14 15:15:22 +0200

    Fix display email, phone

--- frontend/src/components/UserInfoPanel.js
+++ frontend/src/components/UserInfoPanel.js
@@ -15,8 +15,8 @@ const UserInfoPanel = ({ user }) => {
                     {/* name a locality povinne */}
                     <Col className='col-12'> <i className="far fa-id-card fa-fw mr-1"></i> <strong> {user.name} </strong> </Col>
                     
-                    {user.display.email && <Col className='col-12'> <i className="fas fa-at fa-fw mr-1"></i> {user.email} </Col>}
-                    {user.display.phone && <Col className='col-12'> <i className="fas fa-mobile fa-fw mr-1"></i> {user.phoneNumber} </Col> }
+                    {user.display && user.display.email && <Col className='col-12'> <i className="fas fa-at fa-fw mr-1"></i> {user.email} </Col>}
+                    {user.display && user.display.phone && <Col className='col-12'> <i className="fas fa-mobile fa-fw mr-1"></i> {user.phoneNumber} </Col> }
                     <Col className='col-12'> <i className="fas fa-map-marker-alt fa-fw mr-1"></i> {user.locality} </Col>
                 </Row>
             </ListGroup.Item>
